b037116fc6588d75a64cdbf7b2210899d665f1f3
Mliybs <2491988463@qq.com>
2023-02-20 20:01:18 +0800

    update

---
+++ words/index.js
@@ -0,0 +1,11 @@
+fetch('https://mliybs.github.io/words/words.json')
+    .then(res => {
+        console.log(res);
+        return res.json();
+    })
+    .then(returned => {
+        console.log(returned);
+    })
+    .catch(err => {
+        console.log(err);
+    })
