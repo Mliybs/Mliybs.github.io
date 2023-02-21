74aa9f837b5a4fdb305990afd248ce3113322dba
Mliybs <2491988463@qq.com>
2023-02-21 18:44:01 +0800

    update

--- words/index.js
+++ words/index.js
@@ -1,13 +1,16 @@
 var words;
 
+oprate = (callback) => {
+    console.log(callback);
+}
+
 fetch('https://mliybs.github.io/words/words.json')
     .then(res => {
         console.log(res);
         words = res.json();
         return words;
     })
+    .then(oprate(callback))
     .catch(err => {
         console.log(err);
     })
-
-setTimeout(console.log(words), 3000);
